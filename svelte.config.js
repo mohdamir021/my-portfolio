45e7275e476605420e7bbea241a2f01f186378a9
Mohd Amir <amir.ibrahim201200@gmail.com>
2026-04-19 20:49:29 +0800

    core: add netlify deploy build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			edge: false,
+			split: false
 		}),
-    env: {
-      dir: ".env"
-    },
+		env: {
+			dir: '.env'
+		}
 	},
 	preprocess: vitePreprocess()
 };
